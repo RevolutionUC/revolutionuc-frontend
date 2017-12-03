4dcd9d17912bbf64ad053d16bca88213f3d2594a
Kristian Snyder <knsnyder96@gmail.com>
2017-12-02 19:17:19 -0500

    Added smooth scrolling to down chevron
    Added click listener to scroll chevron
    Removes link on chevron if JS runs

---
+++ js/scroll-chevron.js
@@ -0,0 +1,31 @@
+// courtesy of https://stackoverflow.com/a/31987330
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
+
+function scrollChevron() {
+  var downChevronLink = document.querySelector(".down-chevron a");
+  var header = document.querySelector(".header");
+  downChevronLink.removeAttribute("href"); // Clear out the link to the anchor. We'll handle it.
+
+  document
+    .querySelector(".down-chevron a")
+    .addEventListener("click", function(e) {
+      var contentStart = document.getElementById("main");
+      scrollTo(
+        document.documentElement,
+        contentStart.offsetTop - header.clientHeight,
+        250,
+      );
+    });
+}
+
+scrollChevron();
