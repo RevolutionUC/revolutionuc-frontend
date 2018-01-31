501806dcd5978811cde05da8ec89555d75bfe4a7
Kristian Snyder <knsnyder96@gmail.com>
2018-01-31 15:10:21 -0500

    Fixed scroll-chevron for turbolinks & Safari
    Added tracking of module loading for scroll-chevron
    
    Changed scrolling method to be more standards-compliant (fixes it on Safari)

--- js/scroll-chevron.js
+++ js/scroll-chevron.js
@@ -1,33 +1,42 @@
+function currYScrollPos() {
+  return (
+    window.scrollY ||
+    window.pageYOffset ||
+    document.body.scrollTop +
+      ((document.documentElement && document.documentElement.scrollTop) || 0)
+  );
+}
+
 // courtesy of https://stackoverflow.com/a/31987330
-function scrollTo(element, to, duration) {
+function scrollTo(to, duration) {
   if (duration <= 0) return;
-  var difference = to - element.scrollTop;
+  var difference = to - currYScrollPos();
   var perTick = difference / duration * 10;
 
   setTimeout(function() {
-    element.scrollTop = element.scrollTop + perTick;
-    if (element.scrollTop === to) return;
-    scrollTo(element, to, duration - 10);
+    window.scroll(0, currYScrollPos() + perTick);
+    if (currYScrollPos() === to) return;
+    scrollTo(to, duration - 10);
   }, 10);
 }
 
 function scrollChevron() {
   var downChevronLink = document.querySelector(".down-chevron a");
   var header = document.querySelector(".header");
-  downChevronLink.removeAttribute("href"); // Clear out the link to the anchor. We'll handle it.
+  // Clear out the link to the anchor. We'll handle it.
+  downChevronLink.removeAttribute("href");
 
   document
     .querySelector(".down-chevron a")
     .addEventListener("click", function(e) {
       var contentStart = document.getElementById("main");
-      scrollTo(
-        document.documentElement,
-        contentStart.offsetTop - header.clientHeight,
-        250,
-      );
+      scrollTo(contentStart.offsetTop - header.clientHeight, 250);
     });
 }
 
-document.addEventListener("turbolinks:load", function() {
+scroll_chevron_init = false;
+
+if (!scroll_chevron_init) {
   scrollChevron();
-});
+  scroll_chevron_init = true;
+}
