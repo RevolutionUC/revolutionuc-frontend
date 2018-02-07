b3890b39753421b0c3e48dd3e451b58cb7cf2e23
Kristian Snyder <knsnyder96@gmail.com>
2018-02-07 16:46:21 -0500

    Added header hiding (and fixed turbolinks)
    Added a class to animate the header to hidden
    Added a listener that adds and removes the class on scroll
    Fixed the fact that turbolinks wasn't sourced correctly (like, how?)

--- js/header.js
+++ js/header.js
@@ -7,17 +7,23 @@ function header() {
     el.className = el.className.replace(" " + className, "");
   }
 
+  var prev_frame_scroll_position = window.scrollY;
   var last_known_scroll_position = window.scrollY;
   var ticking = false;
   var header = document.querySelector(".header");
   var headerHeight = header.clientHeight;
-  var transition_height =
-    document.querySelector(".hero").getBoundingClientRect().top +
-    window.scrollY -
-    headerHeight;
+
+  // Won't exist on non-root page
+  if (window.location.pathname == "/") {
+    var transition_height =
+      document.querySelector(".hero").getBoundingClientRect().top +
+      window.scrollY -
+      headerHeight;
+  }
+
+  var el = document.querySelector(".header");
 
   function checkScroll(last_known_scroll_position) {
-    var el = document.querySelector(".header");
     if (
       last_known_scroll_position >= transition_height &&
       el.classList.contains("header--no-bg")
@@ -51,9 +57,38 @@ function header() {
     ticking = true;
   }
 
+  var header_animating = false;
+
+  function hideChecker(e) {
+    if (!header_animating) {
+      prev_frame_scroll_position = last_known_scroll_position;
+      last_known_scroll_position = window.scrollY;
+      window.requestAnimationFrame(function() {
+        if (
+          last_known_scroll_position > prev_frame_scroll_position &&
+          last_known_scroll_position > headerHeight / 2 &&
+          !el.classList.contains("header--hidden")
+        ) {
+          addClassTo(el, "header--hidden");
+        } else if (prev_frame_scroll_position > last_known_scroll_position) {
+          removeClassFrom(el, "header--hidden");
+        }
+        window.setTimeout(function() {
+          header_animating = false;
+        }, 100);
+      });
+    }
+
+    header_animating = true;
+  }
+
   // Throttling based on MDN recommendation @
   // https://developer.mozilla.org/en-US/docs/Web/Events/scroll
-  window.addEventListener("scroll", scrollChecker);
+  // Should only be added on root page
+  if (window.location.pathname == "/") {
+    window.addEventListener("scroll", scrollChecker);
+  }
+  window.addEventListener("scroll", hideChecker);
 
   checkScroll(last_known_scroll_position);
 
