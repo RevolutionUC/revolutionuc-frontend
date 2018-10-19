c2b3998f97a1fb06b8d9a7ae87935f0d12924e0d
WillCPP <WillCPP@users.noreply.github.com>
2018-10-19 15:06:03 -0400

    Change colors. Change bubble speed

--- js/experiments/js/app.js
+++ js/experiments/js/app.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 4,
         "direction": "none",
         "random": false,
         "straight": false,
