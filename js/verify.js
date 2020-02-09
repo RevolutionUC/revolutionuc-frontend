e911111ad5d7fd4294e1a71d5edca0f32da76253
WillCPP <WillCPP@users.noreply.github.com>
2020-02-09 17:34:01 -0500

    Update verify.js to grab year from config file

--- js/verify.js
+++ js/verify.js
@@ -1,3 +1,6 @@
+---
+---
+
 function getUrlVars() {
   var vars = {};
   var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
@@ -70,34 +73,37 @@ if (window.location.href.indexOf("attendance") > -1) {
         body: JSON.stringify(jsonData),
       }
     ).then(response => {
-      if (response.status == 200 || response.status == 201) {
+      if (confirm == 'false') {
         document.title = "Confirm Attendance";
         h1.textContent = "Attendance Confirmed";
-        if (confirm == 'true') {
-          content.textContent = "Thank You!  You have confirmed your attendance to RevolutionUC 2020!";
-        } else if (confirm == 'false') {
-          content.textContent = "Thank You!  You have confirmed that you will NOT be attending RevolutionUC 2019.";
-        }
-      } else if (response.status == 500) {
-        response.json().then(body => {
-          if (body.error == "ConfirmedQuotaReached") {
-            document.title = "Waitlist";
-            h1.textContent = "You have been placed on the waitlist.";
-            content.textContent =
-            "The maximum number of people have already confirmed their attendance to RevolutionUC 2020.  As a result you have been placed on the waitlist.";
-          } else {
-            document.title = "Error";
-            h1.textContent = "Error";
-            content.textContent =
+        content.textContent = "Thank You!  You have confirmed that you will NOT be attending RevolutionUC {{ site.data.config.eventData.date.year }}.";
+      } else if (confirm == 'true') {
+        if (response.status == 200 || response.status == 201) {
+          document.title = "Confirm Attendance";
+          h1.textContent = "Attendance Confirmed";
+          content.textContent = "Thank You!  You have confirmed your attendance to RevolutionUC {{ site.data.config.eventData.date.year }}!";
+        } else if (response.status == 500) {
+          response.json().then(body => {
+            if (body.error == "ConfirmedQuotaReached") {
+              document.title = "Waitlist";
+              h1.textContent = "You have been placed on the waitlist.";
+              content.textContent =
+              "The maximum number of people have already confirmed their attendance to RevolutionUC {{ site.data.config.eventData.date.year }}.  As a result you have been placed on the waitlist.";
+            } else {
+              document.title = "Error";
+              h1.textContent = "Error";
+              content.textContent =
+              "There was a problem confirming your attendance.  Please contact us at info@revolutionuc.com.";
+            }
+          })
+        } else {
+          document.title = "Error";
+          h1.textContent = "Error";
+          content.textContent =
             "There was a problem confirming your attendance.  Please contact us at info@revolutionuc.com.";
-          }
-        })
-      } else {
-        document.title = "Error";
-        h1.textContent = "Error";
-        content.textContent =
-          "There was a problem confirming your attendance.  Please contact us at info@revolutionuc.com.";
+        }
       }
-    });
+      }
+      );
   }
 }
