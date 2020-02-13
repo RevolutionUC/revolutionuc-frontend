523c64b1cab4fa05f6c2d6f66c86deb1139c8b60
WillCPP <WillCPP@users.noreply.github.com>
2020-02-13 00:13:08 -0500

    WIP: Live Site

--- js/countdown-timer.js
+++ js/countdown-timer.js
@@ -1,12 +1,15 @@
 function initProgressBar(start, end, time = "day") {
+  // Initialize =======================================
   var barForeground = document.getElementById("barForeground");
   var barBackground = document.getElementById("barBackground");
   var countdownText = document.getElementById("countdownText");
   var startDate = new Date(start);
   var endDate = new Date(end);
   var now = new Date();
+  // ==================================================
 
 
+  // Check times ======================================
   if (now.getTime() < startDate.getTime()) {
     barForeground.style.display = 'none';
     barBackground.style.display = 'none';
@@ -17,10 +20,16 @@ function initProgressBar(start, end, time = "day") {
     countdownText.innerHTML = "Stop Hacking!";
     return;
   }
+  // ==================================================
 
-  // figure out width
 
-  console.log(barBackground.clientWidth);
+  // Calculate start fill =============================
+  var bigDif = endDate.getTime() - startDate.getTime();
+  var dif = endDate.getTime() - now.getTime();
+  var width = 100;
+  var t = bigDif / 100;
+  var startFill = dif / t;
+  width = Math.floor(startFill);
   if (barBackground.clientWidth * (width/100) < 40) {
     barForeground.style.width = 40 + "px";
   } else {
@@ -32,21 +41,15 @@ function initProgressBar(start, end, time = "day") {
     width = Math.floor(startFill);
     barForeground.style.width = width + "%";
   }
-
-  // var bigDif = endDate.getTime() - startDate.getTime();
-  // var dif = endDate.getTime() - now.getTime();
-  // var width = 100;
-  // var t = bigDif / 100;
-  // var startFill = dif / t;
-  // width = Math.floor(startFill);
-  // barForeground.style.width = width + "%";
+  // ==================================================
 
 
+  // Define progress bar frame behavior ===============
   function frame() {
     var now = new Date();
     var timeRemaining = endDate.getTime() - now.getTime();
     var percentFill = timeRemaining / t;
-    if (width <= 0) {
+    if (timeRemaining <= 0) {
       clearInterval(id);
       barForeground.style.width = 0 + "%";
     } else {
@@ -55,25 +58,13 @@ function initProgressBar(start, end, time = "day") {
       } else {
         width = percentFill;
         barForeground.style.width = width + "%";
-        // if (barForeground.clientWidth <= 40 ) {
-        //   barForeground.style.width = 40 + "px";
-        // } else {
-        //   barForeground.style.width = width + "%";
-        // }
       }
     }
   }
-  var textContent = getTimeRemaining();
-  try {
-    countdownText.innerHTML = textContent;
-  } catch (error) {}
-  var id = setInterval(frame, 1000);
-  var id2 = setInterval( function () {
-    var textContent = getTimeRemaining();
-    countdownText.innerHTML = textContent
-  }, 1000)
+  // ==================================================
 
 
+  // Get remaining time as string =====================
   var one_day = 1000 * 60 * 60 * 24;
   var one_hour = 1000 * 60 * 60;
   var one_minute = 1000 * 60;
@@ -98,4 +89,18 @@ function initProgressBar(start, end, time = "day") {
     }
     return textContent;
   }
+  // ==================================================
+
+  
+  // Begin ============================================
+  var textContent = getTimeRemaining();
+  try {
+    countdownText.innerHTML = textContent;
+  } catch (error) {}
+  var id = setInterval(frame, 1000);
+  var id2 = setInterval( function () {
+    var textContent = getTimeRemaining();
+    countdownText.innerHTML = textContent
+  }, 1000)
+  // ==================================================
 }
