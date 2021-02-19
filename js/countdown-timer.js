ead8bf977f321db0d05f0235fb5c36e94fd41e7c
pranavm7 <pranavm7@outlook.com>
2021-02-19 16:09:24 -0500

    Updated the live.html to reflect the updates

--- js/countdown-timer.js
+++ js/countdown-timer.js
@@ -13,7 +13,8 @@ function initProgressBar(start, end, time = "day") {
   if (now.getTime() < startDate.getTime()) {
     barForeground.style.display = 'none';
     barBackground.style.display = 'none';
-    countdownText.style.display = 'none';
+    //countdownText.style.display = 'none';
+    countdownText.innerHTML = "Hacking Begins @ 1pm 02/20 !";
     return;
   } else if (now.getTime() > endDate.getTime()) {
     barForeground.style.width = '0%';
